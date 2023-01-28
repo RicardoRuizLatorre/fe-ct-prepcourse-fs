fbfc770d88da94ad9a3072aec28609d2e9a88d0b
Ricardo Ruiz Latorre <ricardo.ruiz.latorre@gmail.com>
2023-01-27 21:57:14 -0500

    M05 resuelto

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -28,7 +28,6 @@ function incrementarPorUno(array) {
    array.forEach(element => {
          arrayModificado.push(element + 1);
    });
-
    return arrayModificado;
 }
 
@@ -63,36 +62,68 @@ function arrayContiene(array, elemento) {
    // Verifica si el elemento existe dentro del arreglo recibido.
    // Retornar True si está, o False si no está.
    // Tu código:
+   let buscar = array.find(e => e == elemento)
+   if (buscar) return true;
+   else return false;
 }
 
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
+   let sumaResultado = arrayOfNums.reduce((A, B) =>A + B, 0); //
+   return  sumaResultado; 
 }
 
 function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+
+   let contador = 0;
+   for (const i of resultadosTest){
+      contador = contador + i;
+
+   }
+   return contador / resultadosTest.length;
 }
 
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
+   let grande = 0;
+   for (const i of arrayOfNums){
+      if (i > grande) grande = i;
+   }
+   return grande;
 }
 
 function multiplicarArgumentos() {
    // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto.
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
-   // Tu código:
+   // Tu código:   
+   let acc = 1;
+   if (arguments.length === 0) return 0;
+   else if (arguments.length === 1) return arguments [0];
+   else{
+      for(let i =0; i < arguments.length; i++){
+         acc = acc * arguments[1];
+      }
+      return acc;
+   }
 }
 
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
+   let contadorMayorA18 = 0;
+
+   for (const i of array){
+      if (i > 18) contadorMayorA18 ++
+   }
+   return contadorMayorA18;
 }
 
 function diaDeLaSemana(numeroDeDia) {
@@ -100,37 +131,80 @@ function diaDeLaSemana(numeroDeDia) {
    // Realiza una función que, dado el número del día de la semana, retorne: "Es fin de semana"
    // si el día corresponde a "Sábado" o "Domingo", y "Es dia laboral" en caso contrario.
    // Tu código:
+
+   if (numeroDeDia === 1 || numeroDeDia === 7) return "Es fin de semana";
+   else return "Es dia laboral";
+ 
 }
 
 function empiezaConNueve(num) {
    // Esta función recibe por parámetro un número.
    // Debe retornar True si el entero inicia con 9 y False en otro caso.
    // Tu código:
+
+   let txt = num.toString(); //Pasa el numero a string
+   let primerValor = txt.charAt(0);
+
+   if (primerValor === '9') return true;
+   else return false
+
 }
 
 function todosIguales(array) {
    // Si todos los elementos del arreglo son iguales, retornar True.
    // Caso contrario retornar False.
    // Tu código:
-}
 
+   let contadorIgual = 0;
+   let primerElemento = array[0];
+
+   for (let i = 1 ; i < array.length ; i++ ){
+      if (array[i] !== primerElemento ) return false;
+   }
+   return true;
+}
 function mesesDelAño(array) {
    // El arreglo contiene algunos meses del año desordenados. Debes recorrerlo, buscar los meses "Enero",
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   let contadorMeses = 0;
+   var nuevoArray =[];
+   for (const i of array){
+      if (i === "Enero" || i === "Marzo" || i === "Noviembre"){
+         nuevoArray.push(i);
+         contadorMeses++;
+      } 
+   }
+   if (contadorMeses !== 3)return "No se encontraron los meses pedidos"
+   
+   return nuevoArray
 }
 
 function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
    // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
    // Tu código:
+   let arrayTablaDelSeis =[];
+   let contador= 0;
+   while (contador < 11){
+      arrayTablaDelSeis.push(6 * contador);
+      contador++;
+   }
+   return arrayTablaDelSeis;
 }
 
 function mayorACien(array) {
    // La función recibe un arreglo con enteros entre 0 y 200.
    // Recorrerlo y retornar un arreglo con todos los valores mayores a 100 (no incluye el 100).
    // Tu código:
+
+   let arrayMayoresA100 = [];
+   for (const i of array){
+      if (i > 100) arrayMayoresA100.push(i);
+   }
+
+   return arrayMayoresA100;
 }
 
 /* ----------------------------------------------------------------------------------
@@ -144,6 +218,17 @@ function breakStatement(num) {
    // la ejecución y retornar el string: "Se interrumpió la ejecución".
    // [PISTA]: utiliza el statement 'break'.
    // Tu código:
+
+   let arrayIncrementoEnDos = [];
+   let valor = num;
+   for (var i = 0; i < 10; i++){
+      valor = valor + 2;
+
+      if (valor === i) break;
+      else arrayIncrementoEnDos.push(valor);
+   }
+   if (arrayIncrementoEnDos.length < 10) return "Se interrumpió la ejecución"
+   else return arrayIncrementoEnDos;
 }
 
 function continueStatement(num) {
@@ -153,6 +238,22 @@ function continueStatement(num) {
    // se continua con la siguiente iteración.
    // [PISTA]: utiliza el statement 'continue'.
    // Tu código:
+
+   let arrayIncrementoEnDos = [];
+   let valor = num;
+   for (var i = 0; i < 10; i++){
+      
+
+      if (i === 5) {
+         continue
+      }
+      else {
+         valor = valor + 2;
+         arrayIncrementoEnDos.push(valor);
+
+      }
+   }
+   return arrayIncrementoEnDos;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
